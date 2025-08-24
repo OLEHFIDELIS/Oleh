f46b46d6854a4f95f0fb7fcc966efffab4c2e851
Oleh Fidelis <olehfidelis3609@icloud.com>
2025-08-24 09:12:08 +0100

    worked on backend

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelector('.header .nav-bar .nav-list ul ');
+const express = require("express");
+const path = require("path");
 
-const header = document.querySelector('.header.container');
+const app = express();
 
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
-});
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'public')));
 
-document.addEventListener('scroll', () => {
-    var scroll_position = window.scrollY;
-    if (scroll_position > 250) {
-        header.style.backgroundColor = "#29323c";
-    } else {
-        header.style.backgroundColor = 'transparent';
-    }
-});
 
-const navListHandler = function (){
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
-};
-menu_item.onclick = navListHandler;
+app.get("/", (req, res) => {
+   res.sendFile(path.join(__dirname, "public/index.html"));
+})
+
+
+app.listen(3000, () => console.log(`Server running on port 3000`))
+
+
